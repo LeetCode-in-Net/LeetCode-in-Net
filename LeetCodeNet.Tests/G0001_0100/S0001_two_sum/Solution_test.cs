fb78480569509918f55be1bdbb0d1c38a630586c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-18 12:03:12 +0200

    Added task 1

---
+++ LeetCodeNet.Tests/G0001_0100/S0001_two_sum/Solution_test.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeNet.Tests.G0001_0100.S0001_two_sum {
+using System;
+using Xunit;
+
+public class SolutionTest {
+    [Fact]
+    public void TwoSum() {
+        Assert.Equal(new int[] {0, 1}, new Solution().TwoSum(new int[] {2, 7, 11, 15}, 9));
+    }
+
+    [Fact]
+    public void TwoSum2() {
+        Assert.Equal(new int[] {1, 2}, new Solution().TwoSum(new int[] {3, 2, 4}, 6));
+    }
+
+    [Fact]
+    public void TwoSum3() {
+        Assert.Equal(new int[] {0, 1}, new Solution().TwoSum(new int[] {3, 3}, 6));
+    }
+
+    [Fact]
+    public void TwoSum4() {
+        Assert.Equal(new int[] {-1, -1}, new Solution().TwoSum(new int[] {3, 3}, 7));
+    }
+}
+}
---
+++ LeetCodeNet/G0001_0100/S0001_two_sum/Solution.cs
@@ -0,0 +1,24 @@
+namespace G0001_0100.S0001_two_sum {
+
+// #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
+// #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
+// #2023_12_18_Time_134_ms_(81.26%)_Space_44.8_MB_(19.72%)
+
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        Dictionary<int, int> indexMap = new Dictionary<int, int>();
+        for (int i = 0; i < numbers.Length; i++) {
+            int requiredNum = target - numbers[i];
+            if (indexMap.ContainsKey(requiredNum)) {
+                return new int[] {indexMap[requiredNum], i};
+            }
+            indexMap[numbers[i]] = i;
+        }
+        return new int[] {-1, -1};
+    }
+}
+}
+    
