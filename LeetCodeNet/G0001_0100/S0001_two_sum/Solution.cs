329cfd713367607d2197412f9ea10a7328337a41
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-18 17:01:25 +0200

    Fixed namespace

--- LeetCodeNet/G0001_0100/S0001_two_sum/Solution.cs
+++ LeetCodeNet/G0001_0100/S0001_two_sum/Solution.cs
@@ -1,4 +1,4 @@
-namespace G0001_0100.S0001_two_sum {
+namespace LeetCodeNet.G0001_0100.S0001_two_sum {
 
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
 // #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
