0570d4f902625d4edb4f24f8bc56659e463e01f3
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-19 17:32:51 +0200

    Added utils

---
+++ LeetCodeNet.Tests/Com_github_leetcode/LinkedListUtils.cs
@@ -0,0 +1,33 @@
+namespace LeetCodeNet.Com_github_leetcode {
+
+public class LinkedListUtils {
+    public static ListNode ContructLinkedList(int[] nums) {
+        if (nums == null || nums.Length == 0) {
+            return null;
+        }
+        ListNode pre = new ListNode(-1);
+        ListNode head = new ListNode(nums[0]);
+        pre.next = head;
+        for (int i = 1; i < nums.Length; i++) {
+            head.next = new ListNode(nums[i]);
+            head = head.next;
+        }
+        return pre.next;
+    }
+
+    public static ListNode CreateSinglyLinkedList(List<int> listValues) {
+        if (listValues == null || listValues.Count == 0) {
+            throw new ArgumentException(
+                    "Please pass in a valid listValues to create a singly linked list.");
+        }
+        ListNode head = new ListNode(listValues[0]);
+        ListNode tmp = head;
+        for (int i = 1; i < listValues.Count; i++) {
+            ListNode next = new ListNode(listValues[i]);
+            tmp.next = next;
+            tmp = tmp.next;
+        }
+        return head;
+    }
+}
+}
