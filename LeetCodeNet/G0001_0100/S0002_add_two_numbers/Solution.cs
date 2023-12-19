6682e0c7fbfb6c8bcce4064c3651c9b658c55330
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-19 18:02:45 +0200

    Fixed warnings

--- LeetCodeNet/G0001_0100/S0002_add_two_numbers/Solution.cs
+++ LeetCodeNet/G0001_0100/S0002_add_two_numbers/Solution.cs
@@ -5,7 +5,7 @@ using LeetCodeNet.Com_github_leetcode;
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
+        ListNode? p = l1, q = l2, curr = dummyHead;
         int carry = 0;
         while (p != null || q != null) {
             int x = (p != null) ? p.val : 0;
