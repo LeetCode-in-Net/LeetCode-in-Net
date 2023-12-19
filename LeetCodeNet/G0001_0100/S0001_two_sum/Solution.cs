329c62fc65db86617db93b1cf7901f5c964dcc98
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-19 17:11:07 +0200

    Update Solution.cs

--- LeetCodeNet/G0001_0100/S0001_two_sum/Solution.cs
+++ LeetCodeNet/G0001_0100/S0001_two_sum/Solution.cs
@@ -4,9 +4,6 @@ namespace LeetCodeNet.G0001_0100.S0001_two_sum {
 // #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
 // #2023_12_18_Time_134_ms_(81.26%)_Space_44.8_MB_(19.72%)
 
-using System;
-using System.Collections.Generic;
-
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
         Dictionary<int, int> indexMap = new Dictionary<int, int>();
