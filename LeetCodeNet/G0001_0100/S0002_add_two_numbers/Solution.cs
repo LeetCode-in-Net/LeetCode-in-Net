aa72664286c5cc643f7f77cac6a9951ddff40ebc
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-19 17:29:27 +0200

    Added task 2

---
+++ LeetCodeNet/G0001_0100/S0002_add_two_numbers/Solution.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeNet.G0001_0100.S0001_two_sum {
+
+using LeetCodeNet.Com_github_leetcode.ListNode;
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
+}
