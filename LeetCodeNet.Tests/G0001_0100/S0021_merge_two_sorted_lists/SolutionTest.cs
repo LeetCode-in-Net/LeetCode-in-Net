979ba7bb92e5e7f1fb895420322f879c1d7880f6
Valentyn Kolesnikov <valentyn.kolesnikov@dhl.com>
2023-12-26 15:21:52 +0200

    Fixed test

--- LeetCodeNet.Tests/G0001_0100/S0021_merge_two_sorted_lists/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0021_merge_two_sorted_lists/SolutionTest.cs
@@ -6,8 +6,8 @@ using LeetCodeNet.Com_github_leetcode;
 public class SolutionTest {
     [Fact]
     public void MergeTwoLists() {
-        ListNode l1 = LinkedListUtils.CreateSinglyLinkedList(new int[] { 1, 2, 4 });
-        ListNode l2 = LinkedListUtils.CreateSinglyLinkedList(new int[] { 1, 3, 4 });
+        ListNode l1 = LinkedListUtils.CreateSinglyLinkedList(new List<int> { 1, 2, 4 });
+        ListNode l2 = LinkedListUtils.CreateSinglyLinkedList(new List<int> { 1, 3, 4 });
         Assert.Equal("1, 1, 2, 3, 4, 4", new Solution().MergeTwoLists(l1, l2).ToString());
     }
 
