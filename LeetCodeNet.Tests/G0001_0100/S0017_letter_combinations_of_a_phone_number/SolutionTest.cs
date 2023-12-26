99764d72bf8ab82980ca0785a0d2df5a52aed044
Valentyn Kolesnikov <valentyn.kolesnikov@dhl.com>
2023-12-26 15:07:13 +0200

    Fixed test

--- LeetCodeNet.Tests/G0001_0100/S0017_letter_combinations_of_a_phone_number/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0017_letter_combinations_of_a_phone_number/SolutionTest.cs
@@ -1,12 +1,55 @@
 namespace LeetCodeNet.G0001_0100.S0017_letter_combinations_of_a_phone_number {
 
-using System;
 using Xunit;
+using System.Collections.Generic;
 
 public class SolutionTest {
     [Fact]
-    public void Decode() {
-        Assert.Equal("", new Solution().Decode(""));
+    public void LetterCombinations() {
+        Assert.Equal(
+            new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" },
+            new Solution().LetterCombinations("23")
+        );
+    }
+
+    [Fact]
+    public void LetterCombinations2() {
+        Assert.Empty(new Solution().LetterCombinations(""));
+    }
+
+    [Fact]
+    public void LetterCombinations3() {
+        Assert.Equal(new List<string> { "a", "b", "c" }, new Solution().LetterCombinations("2"));
+    }
+
+    [Fact]
+    public void LetterCombinations4() {
+        Assert.Equal(new List<string> { "g", "h", "i" }, new Solution().LetterCombinations("4"));
+    }
+
+    [Fact]
+    public void LetterCombinations5() {
+        Assert.Equal(new List<string> { "j", "k", "l" }, new Solution().LetterCombinations("5"));
+    }
+
+    [Fact]
+    public void LetterCombinations6() {
+        Assert.Equal(new List<string> { "m", "n", "o" }, new Solution().LetterCombinations("6"));
+    }
+
+    [Fact]
+    public void LetterCombinations7() {
+        Assert.Equal(new List<string> { "p", "q", "r", "s" }, new Solution().LetterCombinations("7"));
+    }
+
+    @Fact
+    public void LetterCombinations8() {
+        Assert.Equal(new List<string> { "t", "u", "v" }, new Solution().LetterCombinations("8"));
+    }
+
+    [Fact]
+    public void LetterCombinations9() {
+        Assert.Equal(new List<string> { "w", "x", "y", "z" }, new Solution().LetterCombinations("9"));
     }
 }
 }
