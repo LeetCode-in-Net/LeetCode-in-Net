0f916a14bcae8f42af3615239d28065117120e6f
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-28 10:41:35 +0200

    Fixed tests

--- LeetCodeNet.Tests/G0001_0100/S0046_permutations/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0046_permutations/SolutionTest.cs
@@ -27,7 +27,7 @@ public class SolutionTest {
         Assert.True(CompareLists(expected, actual));
     }
 
-    private bool CompareLists(IList<IList<int>> list1, IList<IList<int>> list2) {
+    private bool CompareLists(List<List<int>> list1, IList<IList<int>> list2) {
         if (list1.Count != list2.Count) return false;
         for (int i = 0; i < list1.Count; i++) {
             if (!list1[i].SequenceEqual(list2[i])) return false;
