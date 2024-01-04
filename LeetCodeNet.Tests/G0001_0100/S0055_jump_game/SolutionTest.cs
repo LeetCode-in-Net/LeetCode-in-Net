a2a7b7eb2b78e3760a94cd5e2c754c1741969b34
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-04 13:31:51 +0200

    Improved tasks 48-55

--- LeetCodeNet.Tests/G0001_0100/S0055_jump_game/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0055_jump_game/SolutionTest.cs
@@ -1,21 +1,17 @@
-namespace LeetCodeNet.G0001_0100.S0055_jump_game
-{
+namespace LeetCodeNet.G0001_0100.S0055_jump_game {
 
-    using System;
-    using Xunit;
+using System;
+using Xunit;
 
-    public class SolutionTest
-    {
-        [Fact]
-        public void CanJump()
-        {
-            Assert.True(new Solution().CanJump(new int[] { 2, 3, 1, 1, 4 }));
-        }
-
-        [Fact]
-        public void CanJump2()
-        {
-            Assert.False(new Solution().CanJump(new int[] { 3, 2, 1, 0, 4 }));
-        }
+public class SolutionTest {
+    [Fact]
+    public void CanJump() {
+        Assert.True(new Solution().CanJump(new int[] { 2, 3, 1, 1, 4 }));
     }
+
+    [Fact]
+    public void CanJump2() {
+        Assert.False(new Solution().CanJump(new int[] { 3, 2, 1, 0, 4 }));
+    }
+}
 }
