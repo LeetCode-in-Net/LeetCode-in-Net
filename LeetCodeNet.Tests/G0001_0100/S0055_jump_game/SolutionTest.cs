e8aa1e48ca55b83e2f444173545c39db5b048b7e
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-04 13:15:21 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0055_jump_game/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0055_jump_game/SolutionTest.cs
@@ -9,13 +9,13 @@ namespace LeetCodeNet.G0001_0100.S0055_jump_game
         [Fact]
         public void CanJump()
         {
-            Assert.Equal(true, new Solution().CanJump(new int[] { 2, 3, 1, 1, 4 }));
+            Assert.True(new Solution().CanJump(new int[] { 2, 3, 1, 1, 4 }));
         }
 
         [Fact]
         public void CanJump2()
         {
-            Assert.Equal(false, new Solution().CanJump(new int[] { 3, 2, 1, 0, 4 }));
+            Assert.False(new Solution().CanJump(new int[] { 3, 2, 1, 0, 4 }));
         }
     }
 }
