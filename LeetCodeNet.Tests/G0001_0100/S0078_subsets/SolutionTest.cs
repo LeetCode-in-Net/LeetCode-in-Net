54e97fe0f2771efe93361d80ac9288d162bba908
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-05 13:26:35 +0200

    Fixed tests

--- LeetCodeNet.Tests/G0001_0100/S0078_subsets/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0078_subsets/SolutionTest.cs
@@ -1,20 +1,21 @@
 namespace LeetCodeNet.G0001_0100.S0078_subsets {
 
-using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SolutionTest {
     [Fact]
-    public void Combine() {
-        var output = new List<List<int>> { new List<int> { 1, 2 }, new List<int> { 1, 3 },
-            new List<int> { 1, 4 }, new List<int> { 2, 3 }, new List<int> { 2, 4 }, new List<int> { 3, 4 } };
-        Assert.Equal(output, new Solution().Combine(4, 2));
+    public void Subsets() {
+        int[][] expected = new int[][] {new int[] {}, new int[] {1},
+            new int[] {1, 2}, new int[] {1, 2, 3}, new int[] {1, 3}, new int[] {2}, new int[] {2, 3}, new int[] {3}};
+        Assert.Equal(expected, new Solution().Subsets(new int[] {1, 2, 3}).Select(a => a.ToArray()).ToArray());
     }
 
     [Fact]
-    public void Combine2() {
-        var output = new List<List<int>> { new List<int> { 1 } };
-        Assert.Equal(output, new Solution().Combine(1, 1));
+    public void Subsets2() {
+        int[][] expected = new int[][] {new int[] {}, new int[] {0}};
+        Assert.Equal(expected, new Solution().Subsets(new int[] {0}).Select(a => a.ToArray()).ToArray());
     }
 }
 }
