42bf96606010238add64ebbf00983b039a72be23
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-05 06:04:48 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0072_edit_distance/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0072_edit_distance/SolutionTest.cs
@@ -1,21 +1,17 @@
-namespace LeetCodeNet.G0001_0100.S0072_edit_distance
-{
+namespace LeetCodeNet.G0001_0100.S0072_edit_distance {
 
-    using System;
-    using Xunit;
+using System;
+using Xunit;
 
-    public class SolutionTest
-    {
-        [Fact]
-        public void MinDistance()
-        {
-            Assert.Equal(3, new Solution().MinDistance("horse", "ros"));
-        }
-
-        [Fact]
-        public void MinDistance2()
-        {
-            Assert.Equal(5, new Solution().MinDistance("intention", "execution"));
-        }
+public class SolutionTest {
+    [Fact]
+    public void MinDistance() {
+        Assert.Equal(3, new Solution().MinDistance("horse", "ros"));
     }
+
+    [Fact]
+    public void MinDistance2() {
+        Assert.Equal(5, new Solution().MinDistance("intention", "execution"));
+    }
+}
 }
