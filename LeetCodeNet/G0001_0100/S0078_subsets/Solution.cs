f191c41de954a1ebf1d2fec74568f7e9bf8820fe
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-05 13:22:01 +0200

    Fixed test

--- LeetCodeNet/G0001_0100/S0078_subsets/Solution.cs
+++ LeetCodeNet/G0001_0100/S0078_subsets/Solution.cs
@@ -4,22 +4,22 @@ namespace LeetCodeNet.G0001_0100.S0078_subsets {
 // #Algorithm_II_Day_9_Recursion_Backtracking #Udemy_Backtracking/Recursion
 // #Big_O_Time_O(2^n)_Space_O(n*2^n) #2024_01_05_Time_101_ms_(94.29%)_Space_45.7_MB_(8.93%)
 
+using Xunit;
 using System.Collections.Generic;
+using System.Linq;
 
-public class Solution {
-    public IList<IList<int>> Subsets(int[] nums) {
-        IList<IList<int>> res = new List<IList<int>>();
-        Solve(nums, new List<int>(), res, 0);
-        return res;
+public class SolutionTest {
+    [Fact]
+    public void Subsets() {
+        int[][] expected = new int[][] {new int[] {}, new int[] {1},
+            new int[] {1, 2}, new int[] {1, 2, 3}, new int[] {1, 3}, new int[] {2}, new int[] {2, 3}, new int[] {3}};
+        Assert.Equal(expected, new Solution().Subsets(new int[] {1, 2, 3}).Select(a => a.ToArray()).ToArray());
     }
 
-    private void Solve(int[] nums, List<int> temp, IList<IList<int>> res, int start) {
-        res.Add(new List<int>(temp));
-        for (int i = start; i < nums.Length; i++) {
-            temp.Add(nums[i]);
-            Solve(nums, temp, res, i + 1);
-            temp.RemoveAt(temp.Count - 1);
-        }
+    [Fact]
+    public void Subsets2() {
+        int[][] expected = new int[][] {new int[] {}, new int[] {0}};
+        Assert.Equal(expected, new Solution().Subsets(new int[] {0}).Select(a => a.ToArray()).ToArray());
     }
 }
 }
