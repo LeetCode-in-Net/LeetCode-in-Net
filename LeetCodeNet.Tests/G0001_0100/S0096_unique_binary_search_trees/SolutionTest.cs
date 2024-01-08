82843ef51df7b21c843ab07d6ddbb71a4f1bbc44
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-08 06:50:17 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0096_unique_binary_search_trees/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0096_unique_binary_search_trees/SolutionTest.cs
@@ -4,12 +4,12 @@ using Xunit;
 
 public class SolutionTest {
     [Fact]
-    public void numTrees() {
+    public void NumTrees() {
         Assert.Equal(5, new Solution().NumTrees(3));
     }
 
     [Fact]
-    public void numTrees2() {
+    public void NumTrees2() {
         Assert.Equal(1, new Solution().NumTrees(1));
     }
 }
