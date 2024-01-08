a1ee6c29d97e94e80f93cd5b0b3ee2a49e9dc9e0
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-08 06:47:48 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0084_largest_rectangle_in_histogram/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0084_largest_rectangle_in_histogram/SolutionTest.cs
@@ -1,18 +1,17 @@
 namespace LeetCodeNet.G0001_0100.S0084_largest_rectangle_in_histogram {
 
-    using System;
-    using Xunit;
+using Xunit;
 
-    public class SolutionTest {
-        [Fact]
-        public void largestRectangleArea() {
-            Assert.Equal(10, new Solution().LargestRectangleArea(new int[] { 2, 1, 5, 6, 2, 3 }));
-        }
-
-        [Fact]
-        public void largestRectangleArea2() {
-            Assert.Equal(4, new Solution().LargestRectangleArea(new int[] { 2, 4 }));
-        }
-
+public class SolutionTest {
+    [Fact]
+    public void largestRectangleArea() {
+        Assert.Equal(10, new Solution().LargestRectangleArea(new int[] { 2, 1, 5, 6, 2, 3 }));
     }
+
+    [Fact]
+    public void largestRectangleArea2() {
+        Assert.Equal(4, new Solution().LargestRectangleArea(new int[] { 2, 4 }));
+    }
+
+}
 }
