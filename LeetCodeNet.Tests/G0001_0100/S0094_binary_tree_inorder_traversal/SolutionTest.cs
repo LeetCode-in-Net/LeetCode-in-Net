d2cd29a98de1bcf9457f0f19988052f433d24151
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-08 06:48:14 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0094_binary_tree_inorder_traversal/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0094_binary_tree_inorder_traversal/SolutionTest.cs
@@ -1,28 +1,27 @@
 namespace LeetCodeNet.G0001_0100.S0094_binary_tree_inorder_traversal {
 
-    using System;
-    using Xunit;
-    using LeetCodeNet.Com_github_leetcode;
+using Xunit;
+using LeetCodeNet.Com_github_leetcode;
 
-    public class SolutionTest {
-        [Fact]
-        public void inorderTraversal() {
-            TreeNode treeNode = new TreeNode(1);
-            TreeNode treeNode2 = new TreeNode(2);
-            treeNode.right = treeNode2;
-            treeNode2.left = new TreeNode(3);
-            Assert.Equal(new List<int> { 1, 3, 2 }, new Solution().InorderTraversal(treeNode));
-        }
-
-        [Fact]
-        public void inorderTraversal2() {
-            Assert.Equal(new List<int> { }, new Solution().InorderTraversal(null));
-        }
-
-        [Fact]
-        public void inorderTraversal3() {
-            Assert.Equal(new List<int> { 1 }, new Solution().InorderTraversal(new TreeNode(1)));
-        }
-
+public class SolutionTest {
+    [Fact]
+    public void inorderTraversal() {
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        treeNode.right = treeNode2;
+        treeNode2.left = new TreeNode(3);
+        Assert.Equal(new List<int> { 1, 3, 2 }, new Solution().InorderTraversal(treeNode));
     }
+
+    [Fact]
+    public void inorderTraversal2() {
+        Assert.Equal(new List<int> { }, new Solution().InorderTraversal(null));
+    }
+
+    [Fact]
+    public void inorderTraversal3() {
+        Assert.Equal(new List<int> { 1 }, new Solution().InorderTraversal(new TreeNode(1)));
+    }
+
+}
 }
