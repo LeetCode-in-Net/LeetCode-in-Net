2266c683e79a6660ff2adebec4701097ef7bc56d
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-09 20:36:13 +0200

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0101_0200/S0142_linked_list_cycle_ii/SolutionTest.cs
+++ LeetCodeNet.Tests/G0101_0200/S0142_linked_list_cycle_ii/SolutionTest.cs
@@ -1,12 +1,11 @@
 namespace LeetCodeNet.G0101_0200.S0142_linked_list_cycle_ii {
 
-using System;
 using Xunit;
 using LeetCodeNet.Com_github_leetcode;
 
 public class SolutionTest {
     [Fact]
-    public void DetectCycleTest1() {
+    public void DetectCycle() {
         ListNode listNode1 = new ListNode(3);
         listNode1.next = new ListNode(2);
         listNode1.next.next = new ListNode(0);
@@ -16,7 +15,7 @@ public class SolutionTest {
     }
 
     [Fact]
-    public void DetectCycleTest2() {
+    public void DetectCycle2() {
         ListNode listNode1 = new ListNode(1);
         listNode1.next = new ListNode(2);
         listNode1.next.next = listNode1;
@@ -24,7 +23,7 @@ public class SolutionTest {
     }
 
     [Fact]
-    public void DetectCycleTest3() {
+    public void DetectCycle3() {
         ListNode listNode1 = new ListNode(1);
         Assert.Equal(null, new Solution().DetectCycle(listNode1));
     }
