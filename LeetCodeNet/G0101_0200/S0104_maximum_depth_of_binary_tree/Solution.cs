7ac524290130bfe7679bb6477f776cf6ef5f0af9
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-09 05:24:17 +0200

    Update Solution.cs

--- LeetCodeNet/G0101_0200/S0104_maximum_depth_of_binary_tree/Solution.cs
+++ LeetCodeNet/G0101_0200/S0104_maximum_depth_of_binary_tree/Solution.cs
@@ -7,19 +7,19 @@ namespace LeetCodeNet.G0101_0200.S0104_maximum_depth_of_binary_tree {
 
 using LeetCodeNet.Com_github_leetcode;
 
-/*
-    * Definition for a binary tree node.
-    * public class TreeNode {
-    *     public int val;
-    *     public TreeNode left;
-    *     public TreeNode right;
-    *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
-    *         this.val = val;
-    *         this.left = left;
-    *         this.right = right;
-    *     }
-    * }
-    */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Solution {
     public int MaxDepth(TreeNode root) {
         return FindDepth(root, 0);
