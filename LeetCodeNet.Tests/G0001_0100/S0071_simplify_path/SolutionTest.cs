d3bef04e53a89cb99c558c6dd73ef9d362b18864
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-04 18:42:00 +0300

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0001_0100/S0071_simplify_path/SolutionTest.cs
+++ LeetCodeNet.Tests/G0001_0100/S0071_simplify_path/SolutionTest.cs
@@ -1,34 +1,36 @@
 namespace LeetCodeNet.G0001_0100.S0071_simplify_path {
-    using Xunit;
-    public class SolutionTest {
-        [Fact]
-        public void SimplifyPath_Example1() {
-            var solution = new Solution();
-            Assert.Equal("/home", solution.SimplifyPath("/home/"));
-        }
 
-        [Fact]
-        public void SimplifyPath_Example2() {
-            var solution = new Solution();
-            Assert.Equal("/", solution.SimplifyPath("/../"));
-        }
+using Xunit;
 
-        [Fact]
-        public void SimplifyPath_Example3() {
-            var solution = new Solution();
-            Assert.Equal("/home/foo", solution.SimplifyPath("/home//foo/"));
-        }
-
-        [Fact]
-        public void SimplifyPath_Example4() {
-            var solution = new Solution();
-            Assert.Equal("/c", solution.SimplifyPath("/a/./b/../../c/"));
-        }
-
-        [Fact]
-        public void SimplifyPath_Complex() {
-            var solution = new Solution();
-            Assert.Equal("/a/b/c", solution.SimplifyPath("/a//b////c/d//././/.."));
-        }
+public class SolutionTest {
+    [Fact]
+    public void SimplifyPath() {
+        var solution = new Solution();
+        Assert.Equal("/home", solution.SimplifyPath("/home/"));
     }
+
+    [Fact]
+    public void SimplifyPath2() {
+        var solution = new Solution();
+        Assert.Equal("/", solution.SimplifyPath("/../"));
+    }
+
+    [Fact]
+    public void SimplifyPath3() {
+        var solution = new Solution();
+        Assert.Equal("/home/foo", solution.SimplifyPath("/home//foo/"));
+    }
+
+    [Fact]
+    public void SimplifyPath4() {
+        var solution = new Solution();
+        Assert.Equal("/c", solution.SimplifyPath("/a/./b/../../c/"));
+    }
+
+    [Fact]
+    public void SimplifyPath5() {
+        var solution = new Solution();
+        Assert.Equal("/a/b/c", solution.SimplifyPath("/a//b////c/d//././/.."));
+    }
+}
 }
