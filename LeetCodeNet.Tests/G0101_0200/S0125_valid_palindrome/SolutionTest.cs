5cf6c7a988eb6a1dd76e8abfb822424c45bb8455
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-10 19:12:02 +0300

    Updated readme

--- LeetCodeNet.Tests/G0101_0200/S0125_valid_palindrome/SolutionTest.cs
+++ LeetCodeNet.Tests/G0101_0200/S0125_valid_palindrome/SolutionTest.cs
@@ -1,21 +1,21 @@
 namespace LeetCodeNet.G0101_0200.S0125_valid_palindrome {
 
-    using Xunit;
+using Xunit;
 
-    public class SolutionTest {
-        [Fact]
-        public void IsPalindrome() {
-            Assert.True(new Solution().IsPalindrome("A man, a plan, a canal: Panama"));
-        }
-
-        [Fact]
-        public void IsPalindrome2() {
-            Assert.False(new Solution().IsPalindrome("race a car"));
-        }
-
-        [Fact]
-        public void IsPalindrome3() {
-            Assert.True(new Solution().IsPalindrome(" "));
-        }
+public class SolutionTest {
+    [Fact]
+    public void IsPalindrome() {
+        Assert.True(new Solution().IsPalindrome("A man, a plan, a canal: Panama"));
     }
+
+    [Fact]
+    public void IsPalindrome2() {
+        Assert.False(new Solution().IsPalindrome("race a car"));
+    }
+
+    [Fact]
+    public void IsPalindrome3() {
+        Assert.True(new Solution().IsPalindrome(" "));
+    }
+}
 }
