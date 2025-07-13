9b714c124deed929989889c67c41e41c13ef14df
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-13 11:28:33 +0300

    Improved task 133

--- LeetCodeNet/G0101_0200/S0133_clone_graph/Node.cs
+++ LeetCodeNet/G0101_0200/S0133_clone_graph/Node.cs
@@ -1,4 +1,5 @@
 namespace LeetCodeNet.G0101_0200.S0133_clone_graph {
+
 public class Node {
     public int val;
     public IList<Node> neighbors;
@@ -20,27 +21,31 @@ public class Node {
 
     public override string ToString() {
         var result = new System.Text.StringBuilder();
-        result.Append("[");
+        result.Append('[');
         bool first = true;
         foreach (var node in neighbors) {
-            if (!first) result.Append(",");
+            if (!first) {
+                result.Append(',');
+            }
             if (node.neighbors == null || node.neighbors.Count == 0) {
                 result.Append(node.val);
             } else {
                 var inner = new System.Text.StringBuilder();
-                inner.Append("[");
+                inner.Append('[');
                 bool innerFirst = true;
                 foreach (var nodeItem in node.neighbors) {
-                    if (!innerFirst) inner.Append(",");
+                    if (!innerFirst) {
+                        inner.Append(',');
+                    }
                     inner.Append(nodeItem.val);
                     innerFirst = false;
                 }
-                inner.Append("]");
+                inner.Append(']');
                 result.Append(inner.ToString());
             }
             first = false;
         }
-        result.Append("]");
+        result.Append(']');
         return result.ToString();
     }
 }
