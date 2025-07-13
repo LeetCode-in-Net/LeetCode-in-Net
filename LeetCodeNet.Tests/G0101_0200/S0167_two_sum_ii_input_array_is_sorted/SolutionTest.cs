301c9a0e76af83e5e402abad87c3ac8924d39d00
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2025-07-13 10:17:10 +0700

    Update SolutionTest.cs

--- LeetCodeNet.Tests/G0101_0200/S0167_two_sum_ii_input_array_is_sorted/SolutionTest.cs
+++ LeetCodeNet.Tests/G0101_0200/S0167_two_sum_ii_input_array_is_sorted/SolutionTest.cs
@@ -1,21 +1,21 @@
 namespace LeetCodeNet.G0101_0200.S0167_two_sum_ii_input_array_is_sorted {
 
-    using Xunit;
+using Xunit;
 
-    public class SolutionTest {
-        [Fact]
-        public void TwoSum() {
-            Assert.Equal(new int[] {1, 2}, new Solution().TwoSum(new int[] {2, 7, 11, 15}, 9));
-        }
-
-        [Fact]
-        public void TwoSum2() {
-            Assert.Equal(new int[] {1, 3}, new Solution().TwoSum(new int[] {2, 3, 4}, 6));
-        }
-
-        [Fact]
-        public void TwoSum3() {
-            Assert.Equal(new int[] {1, 2}, new Solution().TwoSum(new int[] {-1, 0}, -1));
-        }
+public class SolutionTest {
+    [Fact]
+    public void TwoSum() {
+        Assert.Equal(new int[] {1, 2}, new Solution().TwoSum(new int[] {2, 7, 11, 15}, 9));
     }
+
+    [Fact]
+    public void TwoSum2() {
+        Assert.Equal(new int[] {1, 3}, new Solution().TwoSum(new int[] {2, 3, 4}, 6));
+    }
+
+    [Fact]
+    public void TwoSum3() {
+        Assert.Equal(new int[] {1, 2}, new Solution().TwoSum(new int[] {-1, 0}, -1));
+    }
+}
 }
