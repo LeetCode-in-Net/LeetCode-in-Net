25fe42727a220b1e8639dd40eaddee01efc05d86
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2025-07-13 10:02:49 +0700

    update

--- LeetCodeNet.Tests/G0101_0200/S0173_binary_search_tree_iterator/BSTIteratorTest.cs
+++ LeetCodeNet.Tests/G0101_0200/S0173_binary_search_tree_iterator/BSTIteratorTest.cs
@@ -1,25 +1,25 @@
 namespace LeetCodeNet.G0101_0200.S0173_binary_search_tree_iterator {
 
-    using Xunit;
-    using System.Collections.Generic;
-    using LeetCodeNet.Com_github_leetcode;
+using Xunit;
+using System.Collections.Generic;
+using LeetCodeNet.Com_github_leetcode;
 
-    public class BSTIteratorTest {
-        [Fact]
-        public void IteratorBST() {
-            var left = new TreeNode(3);
-            var right = new TreeNode(15, new TreeNode(9), new TreeNode(20));
-            var root = new TreeNode(7, left, right);
-            var iterator = new BSTIterator(root);
-            Assert.Equal(3, iterator.Next());
-            Assert.Equal(7, iterator.Next());
-            Assert.True(iterator.HasNext());
-            Assert.Equal(9, iterator.Next());
-            Assert.True(iterator.HasNext());
-            Assert.Equal(15, iterator.Next());
-            Assert.True(iterator.HasNext());
-            Assert.Equal(20, iterator.Next());
-            Assert.False(iterator.HasNext());
-        }
+public class BSTIteratorTest {
+    [Fact]
+    public void IteratorBST() {
+        var left = new TreeNode(3);
+        var right = new TreeNode(15, new TreeNode(9), new TreeNode(20));
+        var root = new TreeNode(7, left, right);
+        var iterator = new BSTIterator(root);
+        Assert.Equal(3, iterator.Next());
+        Assert.Equal(7, iterator.Next());
+        Assert.True(iterator.HasNext());
+        Assert.Equal(9, iterator.Next());
+        Assert.True(iterator.HasNext());
+        Assert.Equal(15, iterator.Next());
+        Assert.True(iterator.HasNext());
+        Assert.Equal(20, iterator.Next());
+        Assert.False(iterator.HasNext());
     }
+}
 }
--- LeetCodeNet.Tests/G0101_0200/S0188_best_time_to_buy_and_sell_stock_iv/SolutionTest.cs
+++ LeetCodeNet.Tests/G0101_0200/S0188_best_time_to_buy_and_sell_stock_iv/SolutionTest.cs
@@ -1,16 +1,16 @@
 namespace LeetCodeNet.G0101_0200.S0188_best_time_to_buy_and_sell_stock_iv {
 
-    using Xunit;
+using Xunit;
 
-    public class SolutionTest {
-        [Fact]
-        public void MaxProfit() {
-            Assert.Equal(2, new Solution().MaxProfit(2, new int[] {2, 4, 1}));
-        }
-
-        [Fact]
-        public void MaxProfit2() {
-            Assert.Equal(7, new Solution().MaxProfit(2, new int[] {3, 2, 6, 5, 0, 3}));
-        }
+public class SolutionTest {
+    [Fact]
+    public void MaxProfit() {
+        Assert.Equal(2, new Solution().MaxProfit(2, new int[] {2, 4, 1}));
     }
+
+    [Fact]
+    public void MaxProfit2() {
+        Assert.Equal(7, new Solution().MaxProfit(2, new int[] {3, 2, 6, 5, 0, 3}));
+    }
+}
 }
