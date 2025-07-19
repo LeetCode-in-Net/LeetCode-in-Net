fa49da073fb8c2899201bcede7d5a5ee0689abc7
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-19 19:58:32 +0300

    Improved tasks 12, 13, 14

--- LeetCodeNet/G0001_0100/S0013_roman_to_integer/Solution.cs
+++ LeetCodeNet/G0001_0100/S0013_roman_to_integer/Solution.cs
@@ -3,17 +3,13 @@ namespace LeetCodeNet.G0001_0100.S0013_roman_to_integer {
 // #Easy #Top_Interview_Questions #String #Hash_Table #Math #Top_Interview_150_Array/String
 // #Big_O_Time_O(n)_Space_O(1) #2025_06_20_Time_1_ms_(99.81%)_Space_49.42_MB_(80.66%)
 
-public class Solution
-{
-    public int RomanToInt(string s)
-    {
+public class Solution {
+    public int RomanToInt(string s) {
         int x = 0;
         char y;
-        for (int i = 0; i < s.Length; i++)
-        {
+        for (int i = 0; i < s.Length; i++) {
             y = s[i];
-            switch (y)
-            {
+            switch (y) {
                 case 'I':
                     x = GetX(s, x, i, 1, 'V', 'X');
                     break;
@@ -42,8 +38,7 @@ public class Solution
         return x;
     }
 
-    private int GetX(string s, int x, int i, int i2, char v, char x2)
-    {
+    private int GetX(string s, int x, int i, int i2, char v, char x2) {
         if (i + 1 == s.Length)
         {
             x += i2;
