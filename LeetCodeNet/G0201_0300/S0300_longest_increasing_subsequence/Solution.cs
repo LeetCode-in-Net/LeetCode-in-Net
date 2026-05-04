6a67b92da08a764651c3729c74fc62ce2242801a
Valentyn Kolesnikov <javadev75@gmail.com>
2026-05-04 13:32:45 +0300

    Optimize LengthOfLIS implementation

--- LeetCodeNet/G0201_0300/S0300_longest_increasing_subsequence/Solution.cs
+++ LeetCodeNet/G0201_0300/S0300_longest_increasing_subsequence/Solution.cs
@@ -3,44 +3,36 @@ namespace LeetCodeNet.G0201_0300.S0300_longest_increasing_subsequence {
 // #Medium #Top_100_Liked_Questions #Array #Dynamic_Programming #Binary_Search
 // #Algorithm_II_Day_16_Dynamic_Programming #Binary_Search_II_Day_3 #Dynamic_Programming_I_Day_18
 // #Udemy_Dynamic_Programming #Top_Interview_150_1D_DP #Big_O_Time_O(n*log_n)_Space_O(n)
-// #2025_06_16_Time_2_ms_(93.82%)_Space_43.33_MB_(52.97%)
+// #2026_05_04_Time_1_ms_(100.00%)_Space_43.75_MB_(51.72%)
 
 public class Solution {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822", Justification = "LeetCode")]
-    public int LengthOfLIS(int[] nums) {
-        if (nums == null || nums.Length == 0) {
-            return 0;
-        }
-        int[] dp = new int[nums.Length + 1];
-        // prefill the dp table
-        for (int i = 1; i < dp.Length; i++) {
-            dp[i] = int.MaxValue;
-        }
-        int left = 1;
-        int right = 1;
-        foreach (int curr in nums) {
-            int start = left;
-            int end = right;
-            // binary search, find the one that is lower than curr
-            while (start + 1 < end) {
-                int mid = start + (end - start) / 2;
-                if (dp[mid] > curr) {
-                    end = mid;
-                } else {
-                    start = mid;
+    public int LengthOfLIS(int[] nums)
+    {
+        int[] dp = new int[nums.Length];
+        int len = 0;
+        foreach (int x in nums)
+        {
+            int l = 0, r = len;
+            while (l < r)
+            {
+                int m = (l + r) / 2;
+                if (dp[m] < x)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m;
                 }
             }
-            // update our dp table
-            if (dp[start] > curr) {
-                dp[start] = curr;
-            } else if (curr > dp[start] && curr < dp[end]) {
-                dp[end] = curr;
-            } else if (curr > dp[end]) {
-                dp[end] = curr;
-                right++;
+            dp[l] = x;
+            if (l == len)
+            {
+                len++;
             }
         }
-        return right;
+        return len;
     }
 }
 }
